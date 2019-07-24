7c6417be461196bcd57dd22e4b84caeb1cd420ac
Brian Abramian <brianabramian@rocketmail.com>
2019-07-24 16:49:09 -0700

    First few basic commands
    Includes: Kick/ban, unban, ping, 8ball, clear messages

---
+++ my_bot.py
@@ -0,0 +1,85 @@
+import discord
+from discord.ext import commands
+import random
+
+description = ''
+client = commands.Bot(command_prefix='.', description=description)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined the server.')
+
+
+@client.command()
+async def kick(ctx, member : discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+    await ctx.send(f'Kicked {member.mention}')
+
+
+@client.command()
+async def ban(ctx, member : discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+    await ctx.send(f'Banned {member.mention}')
+
+
+@client.command()
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.mention}')
+            return
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
+
+@client.command(aliases=['8ball'])
+async def _8ball(ctx, *, question):
+    replies = ['It is certain',
+            'It is decidedly so',
+            'Without a doubt',
+            'Yes, definitely',
+            'You may rely on it',
+            'As I see it, yes',
+            'Most likely',
+            'Outlook good',
+            'Yes',
+            'Signs point to yes',
+            'Reply hazy, try again',
+            'Ask again later',
+            'Better not tell you now',
+            'Cannot predict now',
+            'Concentrate and ask again',
+            "Don't count on it",
+            'My reply is no',
+            'My sources say no',
+            'Outlook not so good',
+            'Very doubtful']
+    
+    await ctx.send(f'Question: {question}\nAnswer: {replies[random.randint(0, len(replies) - 1)]}')
+    
+
+@client.command()
+async def clear(ctx, amount=5):
+    if amount == 0:
+        return
+    await ctx.channel.purge(limit=amount)
+
+
+
+client.run('NTA3MDU1Njg3OTI3NDYzOTQ1.XTUAKg.5-3-MvgY8L-uQ6SY-DKURNVQrTM')
